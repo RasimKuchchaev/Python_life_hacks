e62848afd6723fb433b65fad46470ccbc57c3dcc
RasimKuchchaev <rasimkuchchaev@gmail.com>
2024-06-21 16:58:37 +0300

    linux_ping_ssh ver 0.4

--- linux_ping_ssh.py
+++ linux_ping_ssh.py
@@ -1,41 +1,43 @@
 # pip install paramiko - ssh client
 
+import os
 import subprocess
 import paramiko
 from paramiko.ssh_exception import NoValidConnectionsError
 
+IP_ADDRESS = '192.168.102.16'
 
 def ping_host(ip_address):
-    try:
-        output = subprocess.check_output(['ping', '-c', '1', ip_address])
+    output = os.system('ping -c 1 ' + ip_address)
+    if output == 0:
+        print(f"Хост доступен {output}")
         return True
-    except subprocess.CalledProcessError:
+    else:
+        print(f"{output} Хост не доступен")
         return False
 
-# ip_address = '192.168.101.232'
-# if ping_host(ip_address):
-#     print(f"Хост доступен {ip_address}")
-# else:
-#     print(f"Хост не доступен {ip_address}")
 
-# for i in range(101, 102):
-#     # print(i)
-#     for j in range(1, 255):
-#         ip_address = "192.168." + str(i) + "." + str(j)
-#         # print(f"192.168.{i}.{j}")
-#         print(ip_address)
-#         if ping_host(ip_address):
-#             print(f"Хост доступен {ip_address}")
+    # try:
+    #     output = subprocess.check_output(['ping', '-c', '1', ip_address])
+    #     return True
+    # except subprocess.CalledProcessError:
+    #     return False
 
 
-def ssh_command_to_host(ip_host, command, title=None):
+def search_host_to_network(subnet_start, subnet_stop):
+    for i in range(subnet_start, subnet_stop + 1):
+        for j in range(1, 255):
+            ip_address = "192.168." + str(i) + "." + str(j)
+            if ping_host(ip_address):
+                print(f"Хост доступен {ip_address}")
+
+
+def ssh_command_to_host(ip_host, command):
     try:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hostname=ip_address, port=22, username='root', password='12345678')
+        ssh.connect(hostname=ip_host, port=22, username='root', password='12345678')
 
-        if title:
-            print(title)
         stdin, stdout, stderr = ssh.exec_command(command)
         stdin.write('Входные данные')
         stdin.flush()
@@ -49,9 +51,9 @@ def ssh_command_to_host(ip_host, command, title=None):
 
 
 def main():
-    ip_address = '192.168.102.16'
     # ping_host(ip_address)
-    ssh_command_to_host(ip_host=ip_address, command="sudo dnf install wine -y", title="install wine")
+    search_host_to_network(subnet_start=96, subnet_stop=102)
+    # ssh_command_to_host(ip_host=IP_ADDRESS, command="sudo dnf install wine -y")
 
 
 if __name__ == '__main__':
