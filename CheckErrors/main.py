87d2c2066a5afc934b4203e27587344262721d78
RasimKuchchaev <rasimkuchchaev@gmail.com>
2024-07-28 23:33:39 +0300

    check error + dz

--- CheckErrors/main.py
+++ CheckErrors/main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import smtplib
 from email.mime.text import MIMEText
 
@@ -23,7 +19,13 @@ def check_errors(file):
     notices = 0
     warnings = 0
     errors = 0
+    # mylist = []
     for line in lines:
+    #    if '[php7:notice]' or '[php7:warn]' or '[php7:error]' in line:
+    #         mylist.append(line.split("]")[-1].strip())
+    # for item in mylist:
+    #     print(item)
+
         if '[php7:notice]' in line:
             notices += 1
         elif '[php7:warn]' in line:
@@ -58,5 +60,3 @@ def send_email(to, subject, message):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     check_errors(FILE)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
